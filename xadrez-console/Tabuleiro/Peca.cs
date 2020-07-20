7f007786e12debaf48c3b2aa03fed6217888e477
Arthur Miranda <arthurdonds@gmail.com>
2020-07-20 11:40:24 -0300

    Começando a colocar pecas no tabuleiro

--- xadrez-console/Tabuleiro/Peca.cs
+++ xadrez-console/Tabuleiro/Peca.cs
@@ -7,9 +7,9 @@
         public int QtdMovimentos { get; protected set; }
         public Tabuleiro Tab { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            Posicao = posicao;
+            Posicao = null;
             Tab = tab;
             Cor = cor;
             QtdMovimentos = 0;
